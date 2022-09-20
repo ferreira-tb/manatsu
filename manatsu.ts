cb07d7bf413415d7d77964269a3c9dd991817772
ferreira-tb <109777783+ferreira-tb@users.noreply.github.com>
2022-09-19 21:14:17 -0300

    Update manatsu.ts

--- manatsu.ts
+++ manatsu.ts
@@ -1,22 +1,21 @@
-type Possibilities = (string | { [index: string]: string } | Element | null)[];
+type AcceptableProperty = (string | Element | { [index: string]: string });
+type ConstructorArgs = (AcceptableProperty | null)[];
+type RepeatConstructor = (AcceptableProperty | number | string[] | null)[];
 
 class Manatsu {
     #element: string = 'div';
-    #options: { [index: string]: string };
-    #parent: Element | undefined;
+    #parent: Element | null = null;
+    #options: { [index: string]: string } | null = null;
 
-    constructor(...args: Possibilities) {
-        for (const arg of args) this.#setProperty(arg);
+    constructor(...args: ConstructorArgs) {
+        for (const arg of args) if (arg !== null && arg !== undefined) this.#setProperty(arg);
     };
 
     #create() {
         const newElement = document.createElement(this.#element);
         
         if (this.#options) {
-            for (const [key, value] of Object.entries(this.#options)) {
-                if (typeof key !== 'string') throw new ManatsuError('option must be a string.');
-                if (typeof value !== 'string') throw new ManatsuError('option must be a string.');
-    
+            for (const [key, value] of Object.entries(this.#options)) {       
                 switch (key) {
                     case 'text': newElement.textContent = value;
                         break;
@@ -35,37 +34,164 @@ class Manatsu {
         return newElement;
     };
 
-    #setProperty(value: any) {
+    #setProperty(value: AcceptableProperty) {
         if (typeof value === 'string') {
-            this.#element = value;
-
-        } else if (isValidObject()) {
-            this.#options = value;
+            if (!Manatsu.isValidElementName(value)) return;
+            this.#element = value.toLowerCase();
 
         } else if (value instanceof Element) {
-            this.#parent = value;
+            if (this.#parent === null) this.#parent = value;
 
-        } else if (value === null || value === undefined) {
-            throw new ManatsuError('value must not be null nor undefined.');
-        };
-
-        function isValidObject() {
-            if (Object.getPrototypeOf(value) === Object.prototype) return true;
-            return false;
+        } else if (Manatsu.isValidOption(value)) {
+            if (this.#options === null) this.#options = value;
         };
     };
 
-    // GETTERS
-    get create() {return this.#create};
+    #addOptions(item: { [index: string]: string } | null) {
+        if (Manatsu.isValidOption(item)) {
+            const oldOptions = { ...this.#options };
+            for (const [attribute, content] of Object.entries(item as object)) {
+                Object.defineProperty(oldOptions, attribute, {
+                    value: content,
+                    enumerable: true
+                });
+            };
+            this.#options = oldOptions;
 
-    // STATIC
-    // Remove todos os filhos de um dado elemento.
-    static #removeChildren(element: HTMLElement) {
+        } else {
+            throw new ManatsuError('O item fornecido é inválido.');
+        };
+    };
+
+    static #repeat(...args: RepeatConstructor): Manatsu[] {
+        const manatsu: Manatsu[] = [];
+        const element: string[] = [];
+        let amount: number = 1;
+        let parent: Element | null = null;
+        let options: { [index: string]: string } | null = null;
+
+        for (const arg of args) {
+            if (arg === null || arg === undefined) continue;
+
+            if (typeof arg === 'number') {
+                if (!Number.isFinite(arg) || arg <= 1 || amount > 1) continue;
+                amount = Math.trunc(arg);
+
+            } else if (typeof arg === 'string') {
+                if (!this.#isValidElementName(arg)) continue;
+                element.push(arg);
+
+            } else if (Array.isArray(arg)) {
+                arg.forEach((item: unknown) => {
+                    if (typeof item === 'string' && Manatsu.isValidElementName(item)) element.push(item);
+                });
+                
+            } else if (arg instanceof Element) {
+                if (parent === null) parent = arg;
+
+            } else if (this.#isValidOption(arg)) {
+                if (options === null) options = arg;
+            };
+        };
+
+        if (element.length === 0) element.push('div');
+        for (let mana = 0; mana < amount; mana++) {
+            const elementType: string = element[mana] ?? element[0];
+            const newElement = new Manatsu(elementType, parent, options);
+            manatsu.push(newElement);
+        };
+
+        return manatsu;
+    };
+
+    static #createAll(manatsu: Manatsu[]): HTMLElement[] {
+        if (!Array.isArray(manatsu)) throw new ManatsuError('O valor fornecido não é uma array.');
+        if (manatsu.length === 0) throw new ManatsuError('A array não pode estar vazia.');
+
+        const collection: HTMLElement[] = [];
+        for (const mana of manatsu) {
+            if (mana instanceof Manatsu) {
+                const newElement = mana.create();
+                collection.push(newElement);
+            };
+        };
+
+        return collection;
+    };
+
+    static #duplicate(item: Manatsu, options?: { [index: string]: string }): Manatsu | undefined {
+        if (item instanceof Manatsu) {
+            const properties: ConstructorArgs = [item.element, item.parent];
+            if (options && this.#isValidOption(options)) {
+                return new Manatsu(...properties, options);
+
+            } else {
+                return new Manatsu(...properties, item.options);
+            };
+
+        } else {
+            throw new ManatsuError('O item não pode ser duplicado.');
+        };
+    };
+
+    static #removeChildren(element: Element) {
         while (element.firstChild) element.removeChild(element.firstChild);
     };
 
-    // STATIC GETTERS
+    static #isValidElementName(name: string) {
+        if (name.length === 0) throw new ManatsuError('O nome do elemento não foi fornecido.');
+        if (!Boolean(name[0].match(/[a-zA-Z]/))) throw new ManatsuError('O nome do elemento precisa iniciar com uma letra (a-z).');
+        return true;
+    };
+
+    static #isValidOption(obj: unknown) {
+        if (Object.getPrototypeOf(obj) === Object.prototype) {
+            for (const [attribute, content] of Object.entries(obj as object)) {
+                if (typeof attribute !== 'string') throw new ManatsuError('O nome do atributo precisa ser uma string.');
+                if (typeof content !== 'string') throw new ManatsuError('O valor do atributo precisa ser uma string.');
+            };
+            return true;
+        };
+        return false;
+    };
+
+    get create() {return this.#create};
+    get addOptions() {return this.#addOptions};
+    get element() {return this.#element};
+    get parent() {return this.#parent};
+    get options() {return this.#options};
+
+    set element(name: string) {
+        if (typeof name === 'string' && Manatsu.isValidElementName(name)) {
+            this.#element = name.toLowerCase();
+        } else {
+            throw new ManatsuError('O nome do elemento precisa ser uma string.');
+        };
+    };
+
+    set parent(parentElement: Element | null) {
+        if (parentElement instanceof Element) {
+            this.#parent = parentElement;
+        } else {
+            throw new ManatsuError('O elemento fornecido é inválido.');
+        };
+    };
+
+    set options(item: { [index: string]: string } | null) {
+        if (Manatsu.isValidOption(item)) {
+            this.#options = item;
+        } else {
+            throw new ManatsuError('O item fornecido é inválido.');
+        };
+    };
+
+    static get repeat() {return this.#repeat};
+    static get createAll() {return this.#createAll};
+    static get duplicate() {return this.#duplicate};
     static get removeChildren() {return this.#removeChildren};
+
+    static get isValidOption() {return this.#isValidOption};
+    static get isValidElementName() {return this.#isValidElementName};
 };
 
 class ManatsuError extends Error {
